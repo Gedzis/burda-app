ebed25ad3ce3fb29755bfee7d64b18d354a1c948
gedisu@gmail.com <gedisu@gmail.com@56ae5688-f550-8b8e-bc54-e2e9bc3727e5>
2012-01-08 20:25:04 +0000

    Maps links

--- trunk/burda-app/src/burda/app/activity/NearestShopsActivity.java
+++ trunk/burda-app/src/burda/app/activity/NearestShopsActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import burda.app.R;
 
+//Via: http://www.vogella.de/articles/AndroidLocationAPI/article.html 
+//http://developer.android.com/resources/tutorials/views/hello-mapview.html
+//http://www.slideshare.net/Maksim_Golivkin/maps-location-api3
 public class NearestShopsActivity extends Activity implements OnClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
