5e26d6d43fb988f26eee47d2cdd1e0019106b945
gedisu@gmail.com <gedisu@gmail.com@56ae5688-f550-8b8e-bc54-e2e9bc3727e5>
2012-01-08 18:45:13 +0000

    Programos rėmai

---
+++ trunk/burda-app/src/burda/app/activity/BrandsActivity.java
@@ -0,0 +1,20 @@
+package burda.app.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import burda.app.R;
+
+public class BrandsActivity extends Activity implements OnClickListener {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.brands);
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+}
