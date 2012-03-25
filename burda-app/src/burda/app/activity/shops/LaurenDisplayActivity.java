791bedcd36e0336f7fda3a240ad7feb53093142a
gedisu@gmail.com <gedisu@gmail.com>
2012-03-25 19:27:49 +0000

    brands click

---
+++ burda-app/src/burda/app/activity/shops/LaurenDisplayActivity.java
@@ -0,0 +1,13 @@
+package burda.app.activity.shops;
+
+import burda.app.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaurenDisplayActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loyalty);
+	}
+}
