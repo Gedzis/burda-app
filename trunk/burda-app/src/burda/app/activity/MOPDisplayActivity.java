8c385ad05330f5bd26f78158cd0f842ac4d2ce4a
KitaSandra@gmail.com <KitaSandra@gmail.com@56ae5688-f550-8b8e-bc54-e2e9bc3727e5>
2012-03-25 19:10:00 +0000

    main screen design

---
+++ trunk/burda-app/src/burda/app/activity/MOPDisplayActivity.java
@@ -0,0 +1,13 @@
+package burda.app.activity;
+
+import burda.app.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MOPDisplayActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loyalty);
+	}
+}
